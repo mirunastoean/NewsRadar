0291dd9c4a9b182214abd35ca16fc5288ff426db
mirunastoean <161870632+mirunastoean@users.noreply.github.com>
2026-07-03 12:00:12 +0300

    Adaugare Kafka in docker-compose si scraper updatat ca Kafka Producer

--- scrapers/rss_scraper.py
+++ scrapers/rss_scraper.py
@@ -1,13 +1,26 @@
-import requests
-import feedparser
+import json
 import time
+import feedparser
+from confluent_kafka import Producer
 
-API_URL = "http://127.0.0.1:8000/articles/"
+
+KAFKA_CONFIG = {
+    'bootstrap.servers': 'localhost:9092'
+}
+TOPIC_NAME = 'rss-articles'
+# API_URL = "http://127.0.0.1:8000/articles/"
 RSS_FEED_URL = "https://www.hotnews.ro/rss"
 SOURCE_NAME = "HotNews"
 
-def fetch_and_save_news():
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Eroare la trimiterea mesajului: {err}")
+    else:
+        print(f"Mesaj trimis cu succes in Kafka pe topicul {msg.topic()}")
+
+def fetch_and_produce_news():
     print(f"Incepem extragerea stirilor de pe: {SOURCE_NAME}...")
+    producer = Producer(KAFKA_CONFIG)
     feed = feedparser.parse(RSS_FEED_URL)
     for entry in feed.entries[:10]:
         article_data={
@@ -16,18 +29,11 @@ def fetch_and_save_news():
             "content": entry.get("summary", "Nu a fost furnizat un rezumat."),
             "source": SOURCE_NAME
         }
-    try:
-        response = requests.post(API_URL, json=article_data)
-        if response.status_code == 200:
-            print(f"Salvat cu succes: {entry.title}")
-        elif response.status_code == 400:
-            print(f"Sarit(exista deja): {entry.title}")
-        else:
-            print(f"Eroare la {entry.title}- Status: {response.status_code}")
-    except requests.exceptions.ConnectionError:
-        print(f"Eroare: Backend-ul nu raspunde. Asigura-te ca uvicorn este pornit!")
-    time.sleep(0.5)
-print("Extragerea s-a finalizat!")
+        json_data = json.dumps(article_data)
+        producer.produce(TOPIC_NAME, value=json_data.encode('utf-8'), callback=delivery_report)
+        time.sleep(0.2)
+    producer.flush()
+    print("Extragerea s-a finalizat!")
 
 if __name__ == "__main__":
-    fetch_and_save_news()
+    fetch_and_produce_news()
