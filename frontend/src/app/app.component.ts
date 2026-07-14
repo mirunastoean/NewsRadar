a300ed085668e4c993830518a3700f0a1e09967e
mirunastoean <161870632+mirunastoean@users.noreply.github.com>
2026-07-14 17:27:42 +0300

    Adaugare search-bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,25 +5,41 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule,MatCardModule, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, CommonModule, MatCardModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'NewsRadar';
   articles: Article[] = [];
-
+  searchTerm: string = '';
+  filteredArticles: Article[] = []; 
+  
   constructor(private articleService: ArticleService) {}
+
   ngOnInit(): void {
     this.articleService.getArticles().subscribe({
       next: (data) => {
         this.articles = data;
+        this.filteredArticles = data; 
         console.log('Articole primite:', this.articles);
       },
       error: (err) => console.error('Eroare la aducerea știrilor:', err)
     });
+  }
+
+  applyFilter() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredArticles = this.articles.filter(article => 
+      article.title.toLowerCase().includes(term) || 
+      article.source.toLowerCase().includes(term)
+    );
   }
 }
